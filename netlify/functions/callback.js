a70fdf943ec1394640a2e6f79979bea1b1904a60
metapillz <iris35137@gmail.com>
2024-11-06 23:58:40 +0900

    Add callback function

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -1,7 +1,8 @@
 exports.handler = async (event, context) => {
-  // 인증 후 처리할 로직
+  console.log("Callback function invoked");
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Google 인증 완료" }),
   };
 };
+
